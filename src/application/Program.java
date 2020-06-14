2a0ee34e7ad158aed5961600492ab45eff169467
ThomasPeruch <peruchthomas@gmail.com>
2020-06-14 11:49:47 -0300

    Comparator como objeto de classe anonima

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -16,7 +15,15 @@ public class Program {
 		list.add(new Product("Smarthphone",1500.00));
 		list.add(new Product("Notebook",1800.00));
 		
-		list.sort(new MyComparator());;
+		Comparator<Product> comp = new Comparator<Product>() {
+			
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}	
+		};
+		
+		list.sort(comp);
 		
 		for(Product p : list) {
 			System.out.println(p);
